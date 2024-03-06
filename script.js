42c6f50b3476898be85b44ca398044c88ae82f06
Artem Starovoitov <myanowitch@yandex.ru>
2024-03-06 22:45:08 +0300

    dadadadadadadada

---
+++ script.js
@@ -0,0 +1,23 @@
+let tg = window.Telegram.WebApp;
+
+
+while(true){
+    let new_block = document.body.createElement('div');
+    new_block.className = "leshinClass";
+
+    
+    let image = new_block.createElement('img');
+
+    image.src = "";
+
+    let upText = new_block.createElement('div');
+    upText.innerHTML="";
+    let downText = new_block.createElement('div');
+    downText.innerHTML="";
+
+    
+    document.body.appendChild(new_block);
+    document.body.new_block.appendChild(image);
+    document.body.new_block.appendChild(upText);
+    document.body.new_block.appendChild(downText);
+}
